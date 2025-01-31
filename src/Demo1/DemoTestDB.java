a6cde040a6fd48aba529814eb8057f0324275467
issam <issam@RPC-2RVWMQ2>
2025-01-30 20:54:29 -0500

    Added new Project: How to Connect to MySQL DB.

---
+++ src/Demo1/DemoTestDB.java
@@ -0,0 +1,52 @@
+package Demo1;
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DemoTestDB {
+	
+	// Main Method
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		// Setup the environment - Query | Select Statement
+		String query = "SELECT * FROM university.students WHERE gpa > '3.40';";
+		
+		// Server Details: URL - UserName - Password
+		String url = "jdbc:mysql://localhost:3306";
+		String username = "root";
+		String password = "123456789";
+		
+		//Import Java SQL Driver Manager
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		// Import Java SQL Connection
+		Connection con = DriverManager.getConnection(url, username, password);
+		
+		// Import Java SQL Statement
+		Statement stm = con.createStatement();
+		
+		// Import Java SQL ResultSet
+		ResultSet rs = stm.executeQuery(query);
+		
+		// While Loop - Loop through the Records
+		while (rs.next()) {
+			System.out.println(rs.getString("first_name"));
+			System.out.println(rs.getString("last_name"));
+			System.out.println(rs.getString("gender"));
+			System.out.println(rs.getString("major"));
+			System.out.println(rs.getString("gpa"));
+			System.out.println("*************");
+		}
+		
+		// Close Connection - Close Statement - Close ResultSet
+		con.close();
+		stm.close();
+		rs.close();
+		
+	}
+
+}
